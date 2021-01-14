b0d41827779ef8816d138d56c94995057992a2d1
Rohit <rohitrangaraj2005@gmail.com>
2021-01-14 15:18:30 +0530

    Firebase Login 0.1

--- Instagram/App.js
+++ Instagram/App.js
@@ -1,17 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as firebase from 'firebase';
+import React, { Component } from 'react'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKQoiNJ-9FgoI06101_ug2cZjMT7DO3fs",
+  authDomain: "instagram-c457c.firebaseapp.com",
+  projectId: "instagram-c457c",
+  storageBucket: "instagram-c457c.appspot.com",
+  messagingSenderId: "1091337235358",
+  appId: "1:1091337235358:web:7905e7ba9218f3549d848f",
+  measurementId: "G-KCHDP9FNJD"
+};
+
+if(firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
+}
+
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'
-import LandingScreen from './components/auth/Landing';
 
-const Stack = createStackNavigator();
-export default function App() {
-  return (
-    <NavigationContainer>      
-      <Stack.Navigator initialRouteName = "Landing">
-        <Stack.Screen name = "Landing" component = { LandingScreen } options = {{ headerShown: false }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  ); 
+import LandingScreen from './components/auth/Landing';
+import RegisterScreen from './components/auth/Register';
+//import LoginScreen from './components/auth/Login'
+
+export class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(( user ) => {
+      if(!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      }
+      else{
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if(!loaded){
+      return(
+        <View style = {{ flex: 1, justifyContent: 'center' }}>
+          <Text>Loading...</Text>
+        </View>
+      )
+    }
+    if(!loggedIn){
+      return (
+        <NavigationContainer>    
+          <Stack.Navigator initialRouteName = "Landing">
+            <Stack.Screen name = "Landing" component = { LandingScreen } options = {{ headerShown: false }}/>
+            <Stack.Screen name = "Register" component = { RegisterScreen } options = {{ headerShown: false }}/>
+            //<Stack.Screen name = "Login" component = { LoginScreen } options = {{ headerShown: false }}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      ); 
+    }
+
+    return(
+      <View style = {{ flex: 1, justifyContent: 'center' }}>
+        <Text>User is Logged In</Text>
+      </View>
+    )
+  }
 }
+
+export default App
