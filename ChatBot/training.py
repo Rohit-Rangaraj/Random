1ff6f297b07759bb8dd01a452d6fc2da2e81aac0
Rohit-Rangaraj <rohitrangaraj2005@gmail.com>
2021-02-07 20:05:18 +0530

    ChatBot 0.1

---
+++ ChatBot/training.py
@@ -0,0 +1,24 @@
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer
+intents = json.loads(open("intents.json").read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ["?", "!", ".", ","]
+
+for intent in intents['intents']:
+    for pattern in intents['patterns']:
+        word_list = nltk.tokenize(pattern)
+        words.append(word_list)
+        documents.append((word_list), )
